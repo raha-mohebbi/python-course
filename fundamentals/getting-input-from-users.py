548bda884e7405262b0bcf0fda201e0f37ea5455
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-02-24 16:35:07 +0330

    how to get input from user

---
+++ fundamentals/getting-input-from-users.py
@@ -0,0 +1,4 @@
+name = input("enter your name: ") #name is variable container that will store the value of the input from the user
+print("hello " + name) #concatenating the string "hello " with the value
+age= input("enter your age: ")
+print("you are " + age + " years old") #concatenating the string "
