d13ea5b5c1544eccb6ee24553996403e91ffb61d
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-02-24 16:42:18 +0330

    this file contains mini projects from tutorial

---
+++ mini_projects/basic_calculator/calculator.py
@@ -0,0 +1,6 @@
+number1= input("enter your first number: ")
+number2= input("enter your second number: ")
+# result= int (number1) + int (number2) #converting the input from string to integer using int() function
+result= float (number1) + float (number2) #converting the input from string to float using float() function
+# result = number1 + number2
+print(result) #this will concatenate the two numbers as strings instead of adding them as integers
