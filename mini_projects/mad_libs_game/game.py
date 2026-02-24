e648a5f006d32187ee06d344bc87af3612b73d2d
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-02-24 16:51:12 +0330

    just a game

---
+++ mini_projects/mad_libs_game/game.py
@@ -0,0 +1,13 @@
+# a game which you can answer random words and it will generate a story for you
+
+# print ("Roses are red")
+# print ("Violets are blue")
+# print ("I love you")
+
+color = input("enter a color: ")
+plural_noun = input("enter a plural noun: ")
+celebrity = input("enter a celebrity: ")
+
+print ("Roses are " + color)
+print (plural_noun + " are blue")
+print ("I love " + celebrity)
