b90634049a96422af91356d43acd9bcb5a90b287
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-02-25 18:58:44 +0330

    dictionaries

---
+++ fundamentals/ditionaries.py
@@ -0,0 +1,9 @@
+monthConversions ={
+    "jan":"january",
+     "feb":"february",
+      "mar":"march"
+}
+print (monthConversions.get("jan")) #this will print january
+print (monthConversions.get("feb")) #this will print february
+print (monthConversions.get("mar")) #this will print march
+print (monthConversions.get("apr")) #this will print none because there is no key "
