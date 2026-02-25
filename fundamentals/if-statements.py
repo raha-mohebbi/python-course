4c15ec423de58e9be6ed982ea59aa40957db0853
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-02-25 16:17:30 +0330

    if & else & elif(else if)

---
+++ fundamentals/if-statements.py
@@ -0,0 +1,9 @@
+is_male=False
+is_tall=True
+
+if is_male or is_tall:  #you can use and instead of or, but in this case we want to check if the person is either male or tall, so we use or. if we use and, then the person would have to be both male and tall, which is not what we want to check for in this case. if we use or, then the person can be either male or tall, or both, and we will print the message if any of those conditions are true. if we use and, then the person would           have to be both male and tall, which is not what we want to check for in this case. if we use or, then the person can be either male or tall, or both, and we will print the message if any of those conditions are true. if we use and, then the person would have to be both male and tall, which is not what we want to check for in this case. if we use or, then the person can be either male or tall, or both, and we will print the message if any of those conditions are true. if we use and, then the person would have to be both male and tall, which is not what we want to check for in this case. if we use or, then the person can be either                                                                               
+    print("you are a male or tall or both")
+else: #elif is used to check for multiple conditions, but in this case we only have one condition to check for, which is if the person is
+   #elif is_male and not (is_tall): #we can use not to check for the opposite of a condition, in this case we are checking if the person
+   
+    print("you are neither male nor tall") 
